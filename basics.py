9660927a053e4d58575c5bef469591fef4e454c3
roullli <rohollah.ehsani@gmail.com>
2025-10-04 12:09:20 +0300

    my first MLOps Journey commit

---
+++ basics.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter a number:\n"))
+num2 = int(input("Enter another number:\n"))
+
+print(f"The sum of the entered numbers: {num1+num2} \n")
+print(f"The difference of the entered numbers: {abs(num1 - num2)} \n")
+print(f"The product of the entered numbers: {num1 * num2} \n")
